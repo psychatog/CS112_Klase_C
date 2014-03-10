1d546ac1afe14939fd995cfdc142312ed0eab223
Clarence <van_scoik@yahoo.com>
2014-03-09 17:26:21 -0700

    week04

---
+++ Echo.java
@@ -0,0 +1,26 @@
+// Clarence Klase
+// 03/09/2014
+// Week04 Homework
+// This class prompts user to enter ints and prints them to screen.
+
+import java.util.Scanner;
+
+public class Echo 
+{
+
+	public static void main( String[] args ) 
+	{
+		Scanner input = new Scanner( System.in );
+		
+		System.out.print( "Please enter a number: " );
+		int aNumber = input.nextInt();
+		
+		System.out.print( "Please enter another number: " );
+		int anotherNumber = input.nextInt();
+		
+		System.out.printf( "Hello, you entered %d and %d.", aNumber, anotherNumber );
+		
+		input.close();
+	}
+
+}
