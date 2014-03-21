2d609ff9d685809795b820f562fabdfa8cf42a11
Clarence <van_scoik@yahoo.com>
2014-03-21 12:00:55 -0700

    Week06 Tester

---
+++ Rock_Paper_Scissors_Tester.java
@@ -0,0 +1,12 @@
+package CS112;
+
+public class Rock_Paper_Scissors_Tester {
+
+	public static void main(String[] args) {
+		
+		Rock_Paper_Scissors game = new Rock_Paper_Scissors();
+		game.runGame();
+		
+	}
+
+}
